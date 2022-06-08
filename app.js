51147641c110b415ec33370717a489b24c138a51
S M Khirul Alam <ka.shagor1@gmail.com>
2022-06-08 10:40:37 +0600

    completed challenge 21 and made the blogapp base version

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const bodyParser =  require("body-parser");
+const _ = require('lodash');
+
 
 const app = express();
+
+let posts = [];
 
 const homeStartingContent =  "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).";
 const aboutContent =  "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.";
@@ -15,7 +19,9 @@ app.use(express.static("public"));
 app.set("view engine","ejs");
 
 app.get("/",(req,res)=>{
-	res.render("home",{content:homeStartingContent});
+	res.render("home",{content:homeStartingContent, posts : posts});
+//	console.log(posts);
+
 });
 
 app.get("/about",(req,res)=>{
@@ -30,13 +36,33 @@ app.get("/compose",(req,res)=>{
 	res.render("compose");
 });
 
+app.get("/posts/:title",(req,res)=>{
+	const reTitle=  _.lowerCase(req.params.title);//requested title
+	posts.forEach((post)=>{
+		const cTitle = _.lowerCase(post.title); //compare title
+		if(cTitle === reTitle){
+				res.render("post",{postTitle:post.title,
+					postConetent:post.content});
+		}
+	})
+});
+
+
+
+
 app.post("/compose",(req,res)=>{
 	const post = {
 	title: req.body.postTitle ,
 	content:req.body.postContent
 };
-	console.log(post);
+	posts.push(post);
+
+	res.redirect("/");
+
 });
+
+
+
 app.listen(3000,()=>{
 	console.log("Server Stated at Port 3000");
 });
